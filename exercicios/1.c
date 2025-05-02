99732dc4593c98e333e89165a6b0aee8cdc41c45
pedrokaut <lexus04111@gmail.com>
2025-05-02 16:03:13 -0300

    primeira
    inicializo a primeira

---
+++ exercicios/1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool ehPrimo(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n1, n2;
+    printf("Digite dois números: ");
+    scanf("%d %d", &n1, &n2);
+    
+    
+    
+    printf("Números primos entre %d e %d são:\n", n1, n2);
+    for (int num = n1; num <= n2; num++) {
+        if (ehPrimo(num)) {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
