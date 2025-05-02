d7014b3667cf492d588723ad89697ee45c894b7e
pedrokaut <lexus04111@gmail.com>
2025-05-02 16:06:12 -0300

    apenas numeros positivos
    estava aceitando numeros menores que um

--- exercicios/1.c
+++ exercicios/1.c
@@ -3,6 +3,9 @@
 #include <locale.h>
 
 bool ehPrimo(int n) {
+    if(n<=1){
+        return false;
+    }
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             return false;
