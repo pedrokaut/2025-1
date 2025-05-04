73a6f33a254a3a6cee4be96ff7a0477de03b0793
pedrokaut <lexus04111@gmail.com>
2025-05-04 18:04:08 -0300

    finalizado
    finalizando a 6 pergunta

--- exercicios/6.c
+++ exercicios/6.c
@@ -1,16 +1,35 @@
-#include<stdio.h>
-int main(){
-    int a[3][3],b[3][3],c[3][3],i,j;
-    printf("preencha a primeira matriz:\n");
-    for ( i = 0; i < 3; i++)
-    {
-        for ( j = 0; j < 3; j++)
-        {
-            printf("digite o elemento [%d][%d]: ",i,j);
-            scanf("%d",&a[i][j]);
+#include <stdio.h>
 
-        }
-        
-    }
+int main() {
+    int a[3][3], b[3][3], c[3][3], i, j;
+
     
+    printf("Preencha a primeira matriz:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("Digite o elemento [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+
+    printf("Preencha a segunda matriz:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("Digite o elemento [%d][%d]: ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    
+    printf("Soma das matrizes:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+            printf("%4d", c[i][j]);  
+        }
+        printf("\n");  
+    }
+
+    return 0;
 }
