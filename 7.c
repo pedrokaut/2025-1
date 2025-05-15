262695c857e06cb1d6f4caac11404f196697f877
pedrokaut <lexus04111@gmail.com>
2025-05-14 22:54:39 -0300

    corrigindo

--- 7.c
+++ 7.c
@@ -1,29 +1,32 @@
 #include<stdio.h>
-struct aluno{
+typedef struct{
     char nome[50];
     int matricula;
     float media;
-};
+}aluno;
 
 int main(){
     int x;
     printf("digite a quantidade de alunos: ");
     scanf("%d", &x);
-    struct aluno alunos[x];
+     aluno alunos[x];
         for(int i=0; i<x; i++){
+            getchar(); 
             printf("digite o  nome do aluno %d: ", i+1);
-            scanf("%s", alunos[i].nome);
+            fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
             printf("digite a matricula do aluno %d: ", i+1);
             scanf("%d", &alunos[i].matricula);
             printf("digite a media do aluno %d: ", i+1);
             scanf("%f", &alunos[i].media);
 
         };
-        printf("alunos cadastrados: \n");
+        printf("alunos aprovados: \n");
         for(int i=0; i<x; i++){
+            if(alunos[i].media >= 7){
             printf("nome: %s\n", alunos[i].nome);
             printf("matricula: %d\n", alunos[i].matricula);
             printf("media: %.2f\n", alunos[i].media);
+            };
 
         };
     
