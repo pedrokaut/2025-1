8220fe08738316452ec5ab19a602aaa96aaaaa3d
pedrokaut <lexus04111@gmail.com>
2025-05-21 16:38:38 -0300

    segunda função inserida

--- exercicios/8.c
+++ exercicios/8.c
@@ -17,5 +17,18 @@ void cadastrarproduto(struct produto *p){
     scanf("%d", &p->codigo);
     printf("digite o preco do produto: ");
     scanf("%f", &p->preco);
+
+}
+void listarprodutos(struct produto *p, int n){
+    printf("produtos cadastrados: \n");
+    for(int i=0; i<n; i++){
+        printf("nome: %s\n", p[i].nome);
+        printf("codigo: %d\n", p[i].codigo);
+        printf("preco: %.2f\n", p[i].preco);
+        printf("\n");
+        
+    }
+
+
 }
 
