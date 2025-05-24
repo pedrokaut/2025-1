bf6f323bde6fecd5b925271e8c80a7523d4e4200
pedrokaut <lexus04111@gmail.com>
2025-05-24 13:45:28 -0300

    corrigindo
    estava quebrado o codigo

--- 10.c
+++ 10.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-int fibonacci(int n);
-int main(){
-    int n;
-    int valor;
-    n = fibonacci(valor);
-    printf("%d\n", n);
-    return 0;
-
-}
 int fibonacci(int n){
-    if(n <=1){
-        return 1;
+    if(n ==1){
+        return 0;
     }else{
-        return fibonacci(n-1)+ fibonacci(n-2);
+        if(n == 2){
+            return 1;
+        }else{
+                return fibonacci(n-1)+ fibonacci(n-2);
+            }
     }
-}
+    }
+int main (){
+    int n;
+    printf("digite o valor para saber o enesimo termo\n");
+    scanf("%d",&n);
+  printf("%d"  ,fibonacci(n));
+};
